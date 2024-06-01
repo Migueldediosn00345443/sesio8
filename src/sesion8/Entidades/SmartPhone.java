0ffd1ff3b62133a84752c5f043131d5a9c3a09cf
MiguelDedios2003 <129539389+Migueldediosn00345443@users.noreply.github.com>
2024-05-31 23:31:39 -0500

    Add files via upload

---
+++ src/sesion8/Entidades/SmartPhone.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion8.Entidades;
+
+import sesion8.Interfacez.Camara;
+import sesion8.Interfacez.Navegable;
+import sesion8.Interfacez.Reproducible;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SmartPhone implements Camara, Navegable, Reproducible {
+
+    @Override
+    public void tomarFoto() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void reproducir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
---
+++ src/sesion8/Entidades/SmartTv.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sesion8.Entidades;
+
+import sesion8.Interfacez.Navegable;
+import sesion8.Interfacez.Reproducible;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SmartTv implements Reproducible, Navegable {
+
+    @Override
+    public void reproducir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void navegar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
